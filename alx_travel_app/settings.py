2d7aa54308c23841dabda157f3014c12e5a79d98
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-18 16:10:25 +0300

    Update settings.py

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -1,1 +1,29 @@
+import environ
+import os
+
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env.bool("DEBUG", default=False)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
+CORS_ORIGIN_ALLOW_ALL = True  # Or set allowed origins with CORS_ORIGIN_WHITELIST
 
